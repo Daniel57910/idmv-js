6d5f3de276b07e43ce45eeb0f8dd59b79be8bd7e
Daniel Miller <danielmiller5791@gmail.com>
2018-05-02 13:14:34 +0100

    pushing updated modules

--- modules/notesCollection.js
+++ modules/notesCollection.js
@@ -1,20 +1,24 @@
 (function(exports) {
-  let notes = [];
   
-  exports.newNote = function(text) {
-    notes.push(text);
-    return this;
-  }
-  
-  exports.allNotes = function() {
-    return notes.map(notes => notes.slice(0,20));
-  }
-  
-  exports.showNote = function(index) {
-    return notes[index];
-  }
-  exports.reset = function() {
-    notes = [];
-    return this;
-  }
+   exports.notesCollection = function() {
+    let notes = [];
+    
+    this.newNote = function(text) {
+      notes.push(text);
+      return this;
+    };
+    
+    this.allNotes = function() {
+      return notes.map(notes => notes.slice(0,20));
+    };
+    
+    this.showNote = function(index) {
+      return notes[index];
+    };
+    
+};
+
+notes = new notesCollection();
+console.log(notes);
+
 })(this);
