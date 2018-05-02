147e304889db6abc0d08e5127e21af772b940471
Irbe Krumina <irbekrm@gmail.com>
2018-05-02 10:24:05 +0100

    encapsulation

--- modules/notesCollection.js
+++ modules/notesCollection.js
@@ -1,21 +1,15 @@
-function notesCollection() {
-  this._notes = [];
-}
-
-notesCollection.prototype.reset = function() {
-  this._notes = [];
-  return this;
-};
-
-notesCollection.prototype.newNote = function(text) {
-  this._notes.push(text);
-  return this;
-};
-
-notesCollection.prototype.allNotes = function() {
-   return this._notes.map(notes => notes.slice(0,20));
-};
-
-module.exports = notesCollection = new notesCollection();
-
-
+(function(exports) {
+  const notes = [];
+  
+  exports.newNote = function(text) {
+    notes.push(text);
+  }
+  
+  exports.allNotes = function() {
+    return notes.map(notes => notes.slice(0,20));
+  }
+  
+  exports.showNote = function(index) {
+    return notes[index];
+  }
+})(this);
