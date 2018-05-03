2e9bf6bfdf2cbf80132ffc325dd35623b37ede8e
Daniel Miller <danielmiller5791@gmail.com>
2018-05-03 11:59:22 +0100

    fixed nnotes - '

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var notes = window.notesCollection;
 window.onload = function () {
   addNewNote();
-
 };
 
 
 function addNewNote() {
   document.getElementById('noteButton').addEventListener('click', function () {
+    console.log("hello");
     let enteredNote = document.getElementById('noteArea').value;
     notes.newNote(enteredNote);
     showAllNotes();
@@ -14,9 +14,14 @@ function addNewNote() {
 }
 
 function showAllNotes() {
+  index = 0;
   let allNotes = notes.allNotes();
-  //allNotes = allNotes.join("<br>");
   console.log(allNotes);
-  document.getElementById('all_notes').innerHTML = allNotes.map((note, index) => "<li id = "+ index">" note+"<br></li>"));
-  //document.getElementById('all_notes').innerHTML = allNotes;
+  document.getElementById('all_notes').innerHTML = allNotes.map((note) => 
+  "<li id = " + index++ + '>' + note + '</li>');
 }
+
+//research event.preventDefault
+//review JS DOM insertion.
+//build a browser/dom, build a bubble.
+//take structured docs, turn into tree then traverse tree.
